2c309f228a49d5d6d96d958eba84a017e2e4957e
Jon <4leite@gmail.com>
2024-05-08 21:17:16 +1200

    change to button action for clarity

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-import Image from "next/image";
 import { getImages } from "./uploadthing";
 
-// We don't have uploadthing secrets at build time
-export const dynamic = 'force-dynamic'
-
 export default async function Home() {
-  const images = await getImages();
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {images.map(({ key, url }) => (
-        <Image key={key} src={url} alt={key} width={200} height={200} />
-      ))}
+      <button onClick={async () => {
+        const images = await getImages();
+        console.log(images);
+      }} />
     </main>
   );
 }
